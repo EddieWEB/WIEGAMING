d5fda2e20a55f0e91e3c5a903724627aedaff5d0
Philip Carlsson <philipcarlsson@hotmail.com>
2021-12-13 11:28:06 +0100

    changing from giphy to unsplash

--- EmptySC.js
+++ EmptySC.js
@@ -1,28 +1,27 @@
-// Skapa en funktion som hämtar en random gif och lägger in den i en div
-// OBS - limited to 42 reads per hour and 1000 searches per day.. :-)
-function randomSadGif() {
+// Skapa en funktion som hämtar en random bild med tema och lägger in den i en div
+function randomSadPic() {
 
     // Create request object 
     const request = new XMLHttpRequest();
 
-    //Give request API key to generate random sad gif from Giphy
-    request.open("get", "https://api.giphy.com/v1/gifs/random?api_key=pvfMt1e2NQwFaNlbRG4k06I1eM5KMM4N&tag=sad&rating=g")
+    //Give request API key to generate random depressed picture from Unsplash
+    request.open("get", "https://api.unsplash.com/photos/random?query=depressed&count=1&client_id=0rx-7YziGErP3yr_pnCYV1G7ecyhOdFcx0N-o-UZXPo")
 
     //send the request
     request.send();
 
-    //Retrieve the request from giphy API
+    //Retrieve the request from unsplash
     request.onload = () => {
         
-        // Convert the object response from API and set that to a constant
+        // Convert the object resp  onse from API and set that to a constant
         const data = JSON.parse(request.response);
         console.log(data)
-        console.log(data.data.url)
+        console.log(data[0].urls.small)
 
-        document.querySelector(".gif").innerHTML = `
-        <img class="random-sad-gif" alt="Random gif from API" src="${data.data.bitly_gif_url}"/>
+       document.querySelector(".emptySC").innerHTML = `
+        <img class="random-sad-gif" alt="Random gif from API" src="${data[0].urls.small}"/>
         `
     } 
 }
 
-document.querySelector("button").addEventListener("click", randomSadGif);
+document.querySelector("button").addEventListener("click", randomSadPic);
