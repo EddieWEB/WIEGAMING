b4da1f5864c793d6afa330b8f26edd8696d68a31
Melissa Zarinnegar <melissahh@live.se>
2021-12-14 14:10:54 +0100

    fixat leveransinformation

--- Checkout.js
+++ Checkout.js
@@ -20,23 +20,20 @@ function payFunction(e){
 e.preventDefault();
 
 
-
+// om input inte fylls i funkar inte knappen (Betalning)
     if(!payWithCart.cardName.value) return;
     if(!payWithCart.cardNumber.value) return;
     if(!payWithCart.cardexpire.value) return;
     if(!payWithCart.cardCvv.value) return;
 
 
-// om input inte fylls i funkar inte knappen
+// om input inte fylls i funkar inte knappen (Dina uppgifter)
     if(!shopperInfo.mail.value) return;
     if(!shopperInfo.firstName.value) return;
     if(!shopperInfo.lastName.value) return;
     if(!shopperInfo.adress.value) return;
     if(!shopperInfo.postCode.value) return;
     if(!shopperInfo.postOrt.value) return;
-
-
-
 
 
     // hämta modal
@@ -62,13 +59,14 @@ e.preventDefault();
     }
     }
  
-    // tömmer input fälten efter click
+
+    // tömmer input fälten efter click (betalning)
     Object.keys(payWithCart).forEach(key => {
 
     payWithCart[key].value = "";     
     });
 
-
+    // tömmer input fälten efter click (dina uppgifter)
     Object.keys(shopperInfo).forEach(key => {
 
         shopperInfo[key].value = "";     
@@ -76,6 +74,7 @@ e.preventDefault();
 }
 
 
+// discoun button lagt till en alert och att inputfältet blir tom efter man tryckt på knappen
 document.querySelector('.btn2').addEventListener('click', discountButton);
 
 const discount = document.querySelector('#discountInput');
@@ -88,5 +87,52 @@ function discountButton(){
         discount[key].value = "";   
 
          });
+
+
 }
+
+
+
+// checkbox leverans och betalning
+function myCheckBox() {
+
+    var checkBoxDelivery = document.getElementById("postnord");
+    var text = document.getElementById("textpostnord");
+
+    if (checkBoxDelivery.checked == true){
+      text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+
+
+    var checkBoxHomeDelivery = document.getElementById("homedelivery");
+    var text = document.getElementById("texthomedelivery");
+
+    if (checkBoxHomeDelivery.checked == true){
+        text.style.display = "block";
+      } else {
+         text.style.display = "none";
+      }
+
+
+      var checkBoxInstaBox = document.getElementById("instabox");
+      var text = document.getElementById("textinstabox");
+  
+      if (checkBoxInstaBox.checked == true){
+          text.style.display = "block";
+        } else {
+           text.style.display = "none";
+        }
+
+        var checkBoxVisaCard = document.getElementById("credit");
+        var text = document.getElementById("textvisacard");
+    
+        if (checkBoxVisaCard.checked == true){
+            text.style.display = "block";
+          } else {
+             text.style.display = "none";
+          }
+
+  }
 
